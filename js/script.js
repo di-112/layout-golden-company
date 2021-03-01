03c195ac39e30410246c927bfd7683bb9ee10a40
di-112 <73027626+di-112@users.noreply.github.com>
2021-03-02 00:02:10 +0300

    added js

--- js/script.js
+++ js/script.js
@@ -6,4 +6,27 @@ $(document).ready(function() {
 	$('.list__item').click( function(event) {
 		if(screen.width <= 700)$('.header__burger,.menu__list').removeClass('active');
 	});
+	$(".menu__list").on("click","a", function (event) {
+		event.preventDefault();
+		let id  = $(this).attr('href'),
+			 top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+  });
+  $(".header__btn").on("click", function (event) {
+		event.preventDefault();
+		let id  = $(this).attr('href'),
+			 top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	});
+	$(window).scroll(function(){
+		if ( $(this).scrollTop() > $(".about").offset().top - 300 ) {
+			$(".about").addClass('animation');
+		}
+	});
+	$(window).scroll(function(){
+		if ( $(this).scrollTop() > $(".services__item").offset().top - 1000 ) {
+			$(".services__item").addClass('animation');
+		}
+	});
+	
 });
